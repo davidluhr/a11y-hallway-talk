c5c948a92dbe65de9dcd8b0189e51aae2c33c6b9
David Luhr <david.luhr@dockyard.com>
2018-12-07 12:01:30 -0700

    Add focus functions and focus cycling

--- app.js
+++ app.js
@@ -1,20 +1,73 @@
 const searchTrigger = document.querySelector('[data-search-trigger]');
 const searchForm = document.querySelector('[data-search-form]');
 const searchInput = document.querySelector('[data-search-input');
+const searchSubmit = document.querySelector('[data-search-submit');
+
+const keyCodeTab = 9;
+const keyCodeEsc = 27;
+
+let searchFormElements = [];
+
+searchFormElements.push(searchInput, searchSubmit);
+
+const focusElement = (element) => {
+  if (element !== undefined) {
+    element.focus();
+  }
+};
+
+const focusPrevious = () => {
+  let entry = document.activeElement;
+  let index = searchFormElements.indexOf(entry);
+
+  do {
+    index--;
+    if (index < 0) {
+      index = searchFormElements.length - 1;
+    }
+    entry = searchFormElements[index];
+  } while (entry.disabled);
+  focusElement(entry);
+};
+
+const focusNext = () => {
+  let entry = document.activeElement;
+  let index = searchFormElements.indexOf(entry);
+
+  do {
+    index++;
+    if (index >= searchFormElements.length) {
+      index = 0;
+    }
+    entry = searchFormElements[index];
+  } while (entry.disabled);
+  focusElement(entry);
+};
 
 // On search trigger click, open search form and move focus to search input
 searchTrigger.addEventListener('click', () => {
   searchForm.classList.add('search--is-open');
-  searchInput.focus();
+  focusElement(searchInput);
 });
 
-// If the search form is open, cycle focus between search input and search submit button
+window.onkeydown = event => {
+  // If the search form is open
+  if (searchForm.classList.contains('search--is-open')) {
+    // Cycle focus between search input and search submit button
+    if (event.keyCode === keyCodeTab) {
+      event.preventDefault();
+      if (event.shiftKey) {
+        focusPrevious();
+      } else {
+        focusNext();
+      }
+    }
 
-
-// If the search form is open and the user presses "esc", close the search form
-window.onkeyup = event => {
-  if (event.keyCode == '27' && searchForm.classList.contains('search--is-open')) {
-    searchForm.classList.remove('search--is-open');
+    // If the user presses "esc", close the search form
+    if (event.keyCode === keyCodeEsc) {
+      event.preventDefault();
+      searchForm.classList.remove('search--is-open');
+    }
   }
 };
 
